29922fed27d59e58dccb204a455ed39ecb14c26f
Hayen Phan <hayenphan@outlook.com>
2020-04-07 14:51:57 +0200

    Added Util folder

---
+++ app/src/main/java/collectipoki/com/util/Constants.java
@@ -0,0 +1,7 @@
+package collectipoki.com.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+}
