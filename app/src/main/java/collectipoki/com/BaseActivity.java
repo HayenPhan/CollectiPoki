049ea471d607e8c383e4632b5b9cee4567867793
Hayen Phan <hayenphan@outlook.com>
2020-04-07 13:00:51 +0200

    Fixed bug

--- app/src/main/java/collectipoki/com/BaseActivity.java
+++ app/src/main/java/collectipoki/com/BaseActivity.java
@@ -9,7 +9,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 // Abstract will make sure that BaseActivity can only be used when extended.
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     // Add Progressbar to this activity
     public ProgressBar mProgressBar;
@@ -17,23 +17,25 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void setContentView(int layoutResID) {
 
-        // ConstraintLayout is the parent
-        // getLayoutInFlater is a class that reads xml appearance description and convert it into java based View objects.
+        /* ConstraintLayout is the parent */
+        /* getLayoutInFlater is a class that reads xml appearance description and convert it into java based View objects. */
         ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
 
-        // Framelayout is inside of Constraintlayout
+        /* Framelayout is inside of Constraintlayout */
         FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
 
-        // Progressbar
+        /* Progressbar */
         mProgressBar = constraintLayout.findViewById(R.id.progress_bar);
 
-        // By doing this anything that extends the BaseActvity will have Framelayout as its container.
+        /* By doing this anything that extends the BaseActvity will have Framelayout as its container. */
         getLayoutInflater().inflate(layoutResID, frameLayout, true);
 
-        super.setContentView(layoutResID);
+        super.setContentView(constraintLayout);
     }
 
-    public void showProgressBar(boolean visibility) {
-        mProgressBar.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
+    public void showProgressBar(boolean visible) {
+        mProgressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
     }
+
 }
+
--- app/src/main/java/collectipoki/com/PokemonListActivity.java
+++ app/src/main/java/collectipoki/com/PokemonListActivity.java
@@ -1,7 +1,4 @@
 package collectipoki.com;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,18 +9,16 @@ public class PokemonListActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Test onclick to see if progress bar is extended
-
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mProgressBar.getVisibility() == View.VISIBLE) {
+                if(mProgressBar.getVisibility() == View.VISIBLE){
                     showProgressBar(false);
-                } else {
+                }
+                else{
                     showProgressBar(true);
                 }
             }
         });
-
     }
 }
